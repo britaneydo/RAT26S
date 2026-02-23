37a7c1bc89c5447ba888d594d7c4f24919298f8e
britaneydo <britaney.do@gmail.com>
2026-02-22 23:31:07 -0800

    deleted class
    scrapped class idea. trying to implement FSM using python functions and dictionaries + table created from NFSM/DFSM

--- lexicon.py
+++ lexicon.py
@@ -1,5 +1,7 @@
 # class definition for NFSM
-class FSM:
+""" class FSM:
+
+    #CSTR
     def __init__(self):
 
         # dictionary for transitions
@@ -23,16 +25,87 @@ class FSM:
         # two layer set (nested dictionary), same symbol can result in different output states
         self.transitions.setdefault(s1, {}).setdefault(symbol, set()).add(s2)
     
+    # s1 --> s2
     def Add_Epsilon(self, s1, s2):
 
         # one layer set since epsilon is essentially no input
         self.epsilon.setdefault(s1, set()).add(s2)
     
+    def Epsilon_Closure(machine, states):
+        
+
+ """
+
+
+
 # =========================================================
 
-# testing code
-transitions = {}
-epsilon = {}
+# DFA table from FSM stored into Python dictionary
+transitions = {
+    'A': {'l': 'B', 'd': None, '_': None},
+    'B': {'l': 'C', 'd': 'D', '_': 'E'},
+    'C': {'l': 'C', 'd': 'D', '_': 'E'},
+    'D': {'l': 'C', 'd': 'D', '_': 'E'},
+    'E': {'l': 'C', 'd': 'D', '_': 'E'},
+}
+
+# all states with state 11 in them (accepting states)
+acceptingStates = {'B', 'C', 'D', 'E'}
+
+startingState = 'A'
+
+# !!! remember to add keywords as needed !!!
+keywords = {'integer', 'if', 'otherwise', 'fi', 'else', 'while', 'for', 'return', 'read', 'write'}
+
+# function to determine char type (letter/digit/underscore)
+def getCharType(c):
+
+    if c.isalpha():
+        return 'l'
+    elif c.isdigit():
+        return 'd'
+    elif c == '_':
+        return '_'
+    
+    # invalid char
+    else:
+        return None
+
+# FSM function to determine if string is valid identifier
+# read string, then check if identifier
+def FSM(s):
+
+    state = startingState
+
+    # go thru every char in string
+    for char in s:
+
+        # determine char type
+        charType = getCharType(char)
+
+        # error handling
+        if charType is None:
+            return False
+        
+        # move onto next state based on char type
+        state = transitions[state].get(charType)
+
+        # error handling
+        if state is None:
+            return False
+    
+    # check if final state is accepting state, return
+    return state in acceptingStates
+
+
+
+
+
+
+
+
+
+
 
 
 
