aaad8fc054e77cd5f846a6d948247c9f20b59f50
britaneydo <britaney.do@gmail.com>
2026-02-22 20:27:23 -0800

    added FSM class

--- lexicon.py
+++ lexicon.py
@@ -1,12 +1,40 @@
-# class definition
+# class definition for NFSM
 class FSM:
     def __init__(self):
 
         # dictionary for transitions
-        self.Transitions = {}
+        # edit with (add_transitions()), ()
+        self.transitions = {}
+        
+        # epsilon transitions
+        self.epsilon = {}
 
         # machine starting state
-        self.StartingState = None
+        self.start = None
+
+        # machine accepting state
+        self.accept = None
+
+    # s1 --symbol--> s2
+    def Add_Transition(self, s1, symbol, s2):
+
+        # input symbol; (s1 --symbol--> s2) if possible
+        # if this symbol does not exist, create it, along with its empty set
+        # two layer set (nested dictionary), same symbol can result in different output states
+        self.transitions.setdefault(s1, {}).setdefault(symbol, set()).add(s2)
+    
+    def Add_Epsilon(self, s1, s2):
+
+        # one layer set since epsilon is essentially no input
+        self.epsilon.setdefault(s1, set()).add(s2)
+    
+# =========================================================
+
+# testing code
+transitions = {}
+epsilon = {}
+
+
 
 
 
